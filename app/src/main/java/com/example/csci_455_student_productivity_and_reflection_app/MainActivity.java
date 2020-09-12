d9f9fb67c3a69af6ca047f800c76d6628c5a4b37
Mary Abigail Garcia <63027685+mary-abigail@users.noreply.github.com>
2020-09-11 23:39:26 -0700

    set up MainActivity

--- app/src/main/java/com/example/csci_455_student_productivity_and_reflection_app/MainActivity.java
+++ app/src/main/java/com/example/csci_455_student_productivity_and_reflection_app/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.csci_455_student_productivity_and_reflection_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startButton = findViewById(R.id.startButton);
+        configureStartButton();
+    }
+
+    private void configureStartButton(){
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+            }
+        });
     }
 }
