81b5719d464fb23ec15e3299ba44a419993f464e
Mary Abigail Garcia <63027685+mary-abigail@users.noreply.github.com>
2020-09-22 16:02:56 -0700

    Bottom Navigation Bar

--- app/src/main/java/com/example/csci_455_student_productivity_and_reflection_app/Dashboard.java
+++ app/src/main/java/com/example/csci_455_student_productivity_and_reflection_app/Dashboard.java
@@ -1,8 +1,13 @@
 package com.example.csci_455_student_productivity_and_reflection_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Dashboard extends AppCompatActivity {
 
@@ -10,5 +15,40 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_mood:
+                            selectedFragment = new MoodFragment();
+                            break;
+                        case R.id.nav_courses:
+                            selectedFragment = new CourseFragment();
+                            break;
+                        case R.id.nav_notes:
+                            selectedFragment = new NotesFragment();
+                            break;
+
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+
+            };
 }
