9f398f4737b0e70262ade313410356d52b03730e
melaniewatson <melaniewatson@users.noreply.github.com>
2020-11-02 00:49:43 -0500

    Signup via emmulator
    ~Finished Authentication set up
    ~Edited signup xml to match buttons in sign up code

--- app/src/main/java/com/example/csci_455_student_productivity_and_reflection_app/Signup.java
+++ app/src/main/java/com/example/csci_455_student_productivity_and_reflection_app/Signup.java
@@ -12,14 +12,22 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Signup extends AppCompatActivity {
 
     private FirebaseAuth sAuth;
-    private EditText sEmail, sPassword;
+   // private FirebaseFirestore fStore;
+    private EditText sName, sEmail, sPassword;
     private Button signupBtn;
 
     @Override
@@ -29,35 +37,45 @@ public class Signup extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
 
         //Get Firebase auth instance
+        //fStore = FirebaseFirestore.getInstance();
         sAuth = FirebaseAuth.getInstance();
+        sName = findViewById(R.id.name);
         sEmail = findViewById(R.id.email);
         sPassword = findViewById(R.id.password);
-        signupBtn = findViewById(R.id.loginButton);
+        signupBtn = findViewById(R.id.signupButton);
 
         signupBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                String name = sName.getText().toString().trim();
                 String email = sEmail.getText().toString().trim();
                 String password = sPassword.getText().toString().trim();
 
-                if (TextUtils.isEmpty(email)) {
+                if (TextUtils.isEmpty(name)) {
+                    Toast.makeText(getApplicationContext(), "Enter your name!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                else if (TextUtils.isEmpty(email)) {
                     Toast.makeText(getApplicationContext(), "Enter an email address!", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
-                if (TextUtils.isEmpty(password)) {
+                else if (TextUtils.isEmpty(password)) {
                     Toast.makeText(getApplicationContext(), "Enter a password!", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
-                if (password.length() < 6) {
+                else if (password.length() < 6) {
                     Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
                     return;
                 }
+                Map<String, Object> userMap = new HashMap<>();
+                userMap.put("accountType", "student");
+                userMap.put("email", email);
+                userMap.put("name", name);
 
                 //create user
-                sAuth.createUserWithEmailAndPassword(email, password)
+               sAuth.createUserWithEmailAndPassword(email, password)
                         .addOnCompleteListener(Signup.this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
@@ -65,15 +83,34 @@ public class Signup extends AppCompatActivity {
                                 // If sign in fails, display a message to the user. If sign in succeeds
                                 // the auth state listener will be notified and logic to handle the
                                 // signed in user can be handled in the listener.
-                                if (!task.isSuccessful()) {
+                                if (task.isSuccessful()) {
+                                    //startActivity(new Intent(Signup.this, Login.class));
+                                    //finish();
+
+                                    Intent intent = new Intent(Signup.this, Dashboard.class);
+                                    startActivity(intent);
+
+                                } else {
                                     Toast.makeText(Signup.this, "Authentication failed." + task.getException(),
                                             Toast.LENGTH_SHORT).show();
-                                } else {
-                                    startActivity(new Intent(Signup.this, Login.class));
-                                    finish();
                                 }
                             }
                         });
+                /**fStore.collection("users")
+                        .add(userMap)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(Signup.this, "Account added to database!", Toast.LENGTH_SHORT).show();
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(Signup.this, "Unable to add account to database!", Toast.LENGTH_SHORT).show();
+                    }
+                });**/
 
             }
 
