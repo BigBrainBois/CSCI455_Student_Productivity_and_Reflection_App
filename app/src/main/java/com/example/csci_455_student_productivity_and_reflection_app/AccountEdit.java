82260f7a93d32381242eefb142ed183610e8d3c4
Mary Abigail Garcia <63027685+mary-abigail@users.noreply.github.com>
2020-11-03 19:00:53 -0800

    Edited Account page to settings, added settings subpages

---
+++ app/src/main/java/com/example/csci_455_student_productivity_and_reflection_app/AccountEdit.java
@@ -0,0 +1,14 @@
+package com.example.csci_455_student_productivity_and_reflection_app;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AccountEdit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+
+}
--- app/src/main/java/com/example/csci_455_student_productivity_and_reflection_app/AccountFragment.java
+++ app/src/main/java/com/example/csci_455_student_productivity_and_reflection_app/AccountFragment.java
@@ -1,18 +1,50 @@
 package com.example.csci_455_student_productivity_and_reflection_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class AccountFragment extends Fragment {
+
+    private RelativeLayout editProfile;
+    private RelativeLayout resetPassword;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        View v = inflater.inflate(R.layout.fragment_account, container, false);
+
+        editProfile = v.findViewById(R.id.menu2);
+        resetPassword = v.findViewById(R.id.menu3);
+        configureProfile();
+        configurePassword();
+        return v;
+    }
+
+    private void configureProfile() {
+        editProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(v.getContext(), AccountEdit.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void configurePassword() {
+        resetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(v.getContext(), ForgotPassword.class);
+                startActivity(intent);
+            }
+        });
     }
 }
