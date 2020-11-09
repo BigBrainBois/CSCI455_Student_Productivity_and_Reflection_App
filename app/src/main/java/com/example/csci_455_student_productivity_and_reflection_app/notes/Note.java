4b2c41cdf43ddad431ba7b1179b702bde04a6392
Destiny Stribling <35977902+dstribli@users.noreply.github.com>
2020-11-09 16:22:44 -0500

    Create Notes is functional. Notes formatting added to Notes Class for Notes Fragment adapter.

--- app/src/main/java/com/example/csci_455_student_productivity_and_reflection_app/notes/Note.java
+++ app/src/main/java/com/example/csci_455_student_productivity_and_reflection_app/notes/Note.java
@@ -1,4 +1,45 @@
 package com.example.csci_455_student_productivity_and_reflection_app.notes;
 
+import java.lang.reflect.Array;
+import java.util.Date;
+
 public class Note {
+
+    private String title;
+    private String subtitle;
+    private String description;
+
+    public Note() {
+
+    }
+
+    public Note(String title, String subtitle, String description) {
+        this.title = title;
+        this.subtitle = subtitle;
+        this.description = description;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
--- app/src/main/java/com/example/csci_455_student_productivity_and_reflection_app/notes/NotesCreate.java
+++ app/src/main/java/com/example/csci_455_student_productivity_and_reflection_app/notes/NotesCreate.java
@@ -1,19 +1,56 @@
 package com.example.csci_455_student_productivity_and_reflection_app.notes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.csci_455_student_productivity_and_reflection_app.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class NotesCreate extends AppCompatActivity {
+
+    private EditText noteTitle, noteSubtitle, noteDescription;
+    private ImageView createNote;
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_note);
+
+        noteTitle = findViewById(R.id.noteTitle);
+        noteSubtitle = findViewById(R.id.noteSubtitle);
+        noteDescription = findViewById(R.id.noteContents);
+
+        createNote = findViewById(R.id.doneButton);
+
+        db = FirebaseFirestore.getInstance();
+
+        createNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //input data
+                String title = noteTitle.getText().toString().trim();
+                String subtitle = noteSubtitle.getText().toString().trim();
+                String description = noteDescription.getText().toString().trim();
+
+                //function call to upload data
+                uploadData(title, subtitle, description);
+            }
+        });
 
         ImageView imageBack = findViewById(R.id.backButton);
         imageBack.setOnClickListener(new View.OnClickListener() {
@@ -21,6 +58,37 @@ public class NotesCreate extends AppCompatActivity {
             public void onClick(View view) {
                 onBackPressed();
             }
+
         });
+    }
+
+    private void uploadData(String title, String subtitle, String description) {
+
+        Map<String, Object> doc = new HashMap<>();
+        doc.put("title", title);
+        doc.put("subtitle", subtitle);
+        doc.put("description", description);
+
+        //add this data
+        db.collection("users").document("coolguy").collection("notes").document().set(doc)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        //called when data is added successfully
+
+                        Toast.makeText(NotesCreate.this, "Saved Successfully. ", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        //if any errors occur while uploading
+
+                        //get and show error message
+                        Toast.makeText(NotesCreate.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
     }
 }
